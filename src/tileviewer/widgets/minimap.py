c72694d65bf67b64c9d317d1651d6e0cf60a2f3d
Alexander Becker <abecker@qass.office>
2026-02-11 20:16:40 +0100

    feat: Moving the scrollbar now updates the selection in the minimap.

--- src/tileviewer/widgets/minimap.py
+++ src/tileviewer/widgets/minimap.py
@@ -10,18 +10,16 @@ class Minimap(QtWidgets.QWidget):
         self.tilemap_pixmap = None
         self.scaled_tilemap_pixmap = None
 
-        self.rubberband = QtWidgets.QRubberBand(QtWidgets.QRubberBand.Shape.Rectangle, self)
+        self.rubber_band = QtWidgets.QRubberBand(QtWidgets.QRubberBand.Shape.Rectangle, self)
 
-        self.rubberband_height = 0
-        self.rubberband.setFixedHeight(self.rubberband_height)
-        self.rubberband.setFixedWidth(140)
+        self.rubber_band_height = 0
+        self.rubber_band.setFixedHeight(self.rubber_band_height)
+        self.rubber_band.setFixedWidth(140)
 
         self.setFixedWidth(140)
         self.setMinimumHeight(200)
 
-        # TODO connect to function that oves rubberband
-        self.scroll_area.verticalScrollBar().valueChanged.connect(self.update)
-        self.scroll_area.horizontalScrollBar().valueChanged.connect(self.update)
+        self.scroll_area.verticalScrollBar().valueChanged.connect(self.update_rubber_band_position)
 
         self._is_clicked = False
 
@@ -35,42 +33,48 @@ class Minimap(QtWidgets.QWidget):
         )
         self.image_preview.setPixmap(self.scaled_tilemap_pixmap)
 
-        relative_rubberband_height = self.scroll_area.viewport().height() / self.tilemap_pixmap.height()
-        self.rubberband_height = int(self.height() * relative_rubberband_height)
-        self.rubberband.setFixedHeight(self.rubberband_height)
-        self.rubberband.show()
+        # TODO update position of rubber band
+        relative_rubber_band_height = self.scroll_area.viewport().height() / self.tilemap_pixmap.height()
+        self.rubber_band_height = int(self.height() * relative_rubber_band_height)
+        self.rubber_band.setFixedHeight(self.rubber_band_height)
+        self.rubber_band.show()
 
     def paintEvent(self, event: QtGui.QPaintEvent) -> None:  # noqa: ARG002, N802
         if self.scaled_tilemap_pixmap is not None:
             painter = QtGui.QPainter(self)
             painter.drawPixmap(0, 0, self.scaled_tilemap_pixmap)
 
+    def update_rubber_band_position(self, scroll_bar_position: int) -> None:
+        if self.tilemap_pixmap is not None and not self._is_clicked:
+            relative_scroll_bar_position = scroll_bar_position / self.tilemap_pixmap.height()
+            rubber_band_position_y = relative_scroll_bar_position * self.height()
+            rubber_band_position_y = min(rubber_band_position_y, self.height() - 0.5 * self.rubber_band_height)
+            self.move_rubber_band(int(rubber_band_position_y))
+
     def scroll_to_selction(self) -> None:
         if self.tilemap_pixmap is not None:
-            rubberband_position = self.rubberband.pos()
-            relative_rubberband_position = rubberband_position.y() / self.height()
-            scroll_position = self.tilemap_pixmap.height() * relative_rubberband_position
+            rubber_band_position = self.rubber_band.pos()
+            relative_rubber_band_position = rubber_band_position.y() / self.height()
+            scroll_position = self.tilemap_pixmap.height() * relative_rubber_band_position
             self.scroll_area.verticalScrollBar().setValue(int(scroll_position))
 
-    def update_selection_rectanlge_position(self, position: QtCore.QPoint) -> None:
-        y = max(position.y() - 0.5 * self.rubberband_height, 0)
-        y = min(y, self.height() - self.rubberband_height)
-        position.setY(int(y))
-
-        position.setX(0)
-        self.rubberband.move(position)
-        self.scroll_to_selction()
+    def move_rubber_band(self, y_rubber_band_center: int) -> None:
+        y_rubber_band_top = max(y_rubber_band_center - 0.5 * self.rubber_band_height, 0)
+        y_rubber_band_top = min(y_rubber_band_top, self.height() - self.rubber_band_height)
+        self.rubber_band.move(0, int(y_rubber_band_top))
 
     def mousePressEvent(self, event: QtGui.QMouseEvent) -> None:  # noqa: N802
         self._is_clicked = True
-        self.update_selection_rectanlge_position(event.pos())
+        self.move_rubber_band(event.pos().y())
+        self.scroll_to_selction()
 
     def mouseReleaseEvent(self, event: QtGui.QMouseEvent) -> None:  # noqa: ARG002, N802
         self._is_clicked = False
 
     def mouseMoveEvent(self, event: QtGui.QMouseEvent) -> None:  # noqa: N802
         if self._is_clicked:
-            self.update_selection_rectanlge_position(event.pos())
+            self.move_rubber_band(event.pos().y())
+            self.scroll_to_selction()
 
     def resizeEvent(self, event: QtGui.QResizeEvent) -> None:  # noqa: ARG002, N802
         if self.tilemap_pixmap is not None:
